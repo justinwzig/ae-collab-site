5e99982337a9d34c4467b3d97e1c6bd7da2b8532
Justin Ziegelmueller <justinwzig@gmail.com>
2017-06-18 12:12:59 -0500

    updates

---
+++ npm-install.bat
@@ -0,0 +1,8 @@
+@echo off
+start npm install -D gulp
+start npm install -D gulp-nodemon
+start npm install -D gulp-pug
+start npm install -D gulp-coffee
+start npm install -D gulp-uglify
+start npm install -D gulp-sass
+start npm install -D gulp-clean-css
